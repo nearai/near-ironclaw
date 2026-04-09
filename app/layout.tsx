da82f63e018e16452902242112a691a244cca551
StrykerUX <abraham.stryker117@gmail.com>
2026-04-09 11:24:49 -0600

    feat: add Google Ads conversion tracking to Deploy buttons (#37)

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,22 @@ export default function RootLayout({
             gtag('config', 'AW-17691708623');
           `}
         </Script>
+        <Script id="google-ads-conversion" strategy="afterInteractive">
+          {`
+            function gtag_report_conversion(url) {
+              var callback = function () {
+                if (typeof(url) != 'undefined') {
+                  window.location = url;
+                }
+              };
+              gtag('event', 'conversion', {
+                'send_to': 'AW-17691708623/99PrCPjJopgcEM-ZiPRB',
+                'event_callback': callback
+              });
+              return false;
+            }
+          `}
+        </Script>
       </head>
       <body className={`${fkGrotesk.variable} ${fkGroteskMono.variable} antialiased`}>
         {process.env.NEXT_PUBLIC_POSTHOG_KEY ? (
